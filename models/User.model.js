ac1d19e0e128beef41954ffdbb60a41a57ab40da
Florian Becker <becker_florian@online.de>
2021-09-09 21:04:39 +0200

    created Usermodel

---
+++ models/User.model.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  }, 
+  lastName: {
+    type: String,
+    required: true
+  },
+  eMail: {
+    type: String, 
+    required: true, 
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String, 
+    required: true
+  },
+  profilePicture: {
+    type: String,
+    default: "https://pixabay.com/vectors/avatar-beak-black-cute-emotion-1295404/"
+  }
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("buena-vista-streetart-club JS imported successfully!");
+  },
+  false
+);

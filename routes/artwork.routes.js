fa905536ceff114b5e173c7bafcfedbc4f53a1fa
Florian Becker <becker_florian@online.de>
2021-09-21 22:28:21 +0200

    changed the res.redirect in artwork/delete

--- routes/artwork.routes.js
+++ routes/artwork.routes.js
@@ -129,7 +129,7 @@ router.post("/details/:artworkId/delete", isLoggedIn, (req, res, next) => {
   const { artworkId } = req.params;
   Art.findByIdAndDelete(artworkId)
     .then(() => {
-      res.redirect("/list");
+      res.redirect("/overview");
     })
     .catch((err) => {
       console.log(err);
